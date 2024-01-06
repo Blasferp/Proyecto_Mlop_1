d56cdb9baeeaf5d721adcf1d3f9b433a219681bc
Blas Fernando Pacios <blasferp@gmail.com>
2024-01-06 12:20:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import pickle
 
 app = FastAPI()
 
+from fastapi import FastAPI, HTMLResponse
+
+app = FastAPI()
+
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     mensaje = """
@@ -16,27 +20,28 @@ def read_root():
             <head>
                 <style>
                     body {
-                        background-image: url('Image\steam.jpg');
+                        background-image: url('https://ejemplo.com/imagen.jpg');
                         background-size: cover;
                         background-position: center;
-                        height: 100vh; /* Ajusta la altura según sea necesario */
-                        margin: 0; /* Elimina el margen por defecto del body */
+                        height: 100vh;
+                        margin: 0;
                         display: flex;
                         flex-direction: column;
                         justify-content: center;
                         align-items: center;
-                        color: white; /* Ajusta el color del texto según sea necesario */
+                        color: white;
                     }
                 </style>
             </head>
             <body>
                 <h1>API Consulta y recomendación de videojuegos Steam</h1>
                 <h2>Bienvenidos a la API del Proyecto N° 1 Machine Learning operations</h2>
-                <p>Puede ingresar agregando \Docs a continuación de la URL actual de esta página para interactuar con la API</p>
+                <p>Puede ingresar agregando \Dcs a continuación de la URL actual de esta página para interactuar con la API</p>
             </body>
         </html>
     """
     return HTMLResponse(content=mensaje, status_code=200)
+
 
 @app.get("/readme", response_class=HTMLResponse)
 def get_readme():
